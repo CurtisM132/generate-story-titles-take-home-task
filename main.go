17053705be3e48b4f31d8477fc631ba7aa1635ae
Curtis Martin <curtisastonmartin@hotmail.co.uk>
2022-04-10 12:32:19 +0100

    Rudimentary Story Title Generation

--- main.go
+++ main.go
@@ -37,27 +37,33 @@ func main() {
 			}
 
 			if len(records) > 0 {
-				storyTitle, err := generateStoryTitle(records, geocoder)
-				if err != nil {
-					log.Printf("failed to generate story title for dataset - %s", err)
-					continue
-				}
-				storyTitles = append(storyTitles, storyTitle)
+				storyTitles = append(storyTitles, generateStoryTitle(records, geocoder))
 			}
 		}
 	}
 
-	// for _, storyTitle := range storyTitles {
-	// 	fmt.Println(storyTitle)
-	// }
+	for _, storyTitle := range storyTitles {
+		fmt.Println(storyTitle)
+	}
 }
 
-func generateStoryTitle(records []*csv.Record, geocoder *reversegeocoder.ReverseGeocoder) (string, error) {
+func generateStoryTitle(records []*csv.Record, geocoder *reversegeocoder.ReverseGeocoder) string {
+	storyTitle := generateTripStoryTitleComponent(records)
+	storyTitle = storyTitle + generateLocationStoryTitleComponent(records, geocoder)
+
+	return storyTitle
+}
+
+func generateTripStoryTitleComponent(records []*csv.Record) string {
 	tripLength, err := calculateTripLength(records)
 	if err != nil {
-		log.Printf("failed to calculate trip length - %s - falling back to default", err)
+		log.Printf("failed to calculate trip length - %s - falling back to default of %d", err, tripLength)
 	}
 
+	return "A trip to "
+}
+
+func generateLocationStoryTitleComponent(records []*csv.Record, geocoder *reversegeocoder.ReverseGeocoder) string {
 	coords := []geospatial.Coordinates{}
 	for i := range records {
 		coord := geospatial.Coordinates{
@@ -77,9 +83,14 @@ func generateStoryTitle(records []*csv.Record, geocoder *reversegeocoder.Reverse
 		log.Printf("failed to get trip location - %s", err)
 	}
 
-	fmt.Println(tripLength, locationSpread, tripLocation)
-
-	return "blah", nil
+	switch {
+	case locationSpread > 15000:
+		return tripLocation.Country
+	case locationSpread > 10000:
+		return tripLocation.Locality
+	default:
+		return tripLocation.Sublocality
+	}
 }
 
 // calculateTripLength Takes in a set of CSV records and returns the largest trip length (in days)
@@ -88,12 +99,12 @@ func calculateTripLength(records []*csv.Record) (int, error) {
 	timeBetweenInDays := func(layout string) (int, error) {
 		startDate, err := time.Parse(layout, records[0].Datetime)
 		if err != nil {
-			return 0, fmt.Errorf("failed to parse time - %s", err)
+			return 1, fmt.Errorf("failed to parse time - %s", err)
 		}
 
 		endDate, err := time.Parse(layout, records[len(records)-1].Datetime)
 		if err != nil {
-			return 0, fmt.Errorf("failed to parse time - %s", err)
+			return 1, fmt.Errorf("failed to parse time - %s", err)
 		}
 
 		return int(math.Abs(math.Ceil(endDate.Sub(startDate).Hours() / 24))), nil
@@ -117,8 +128,10 @@ func calculateTripLength(records []*csv.Record) (int, error) {
 // then returns the human readable location of that midpoint (e.g., New York, Manhattan, United States)
 func getTripLocation(coordDataset []geospatial.Coordinates, geocoder *reversegeocoder.ReverseGeocoder) (
 	geospatial.Location, error) {
+	// Get the mid point of the dataset so we can use that as the location to reverse geocode
 	midPoint, err := geospatial.CalculateMidPoint(coordDataset)
 	if err != nil {
+		// If we fail for whatever reason then use the location from the middle of the dataset
 		midPoint.Lat = coordDataset[len(coordDataset)/2].Lat
 		midPoint.Lon = coordDataset[len(coordDataset)/2].Lon
 		log.Printf("failed to calculate geographical midpoint of the trip - %s - falling back to %f, %f", err,
